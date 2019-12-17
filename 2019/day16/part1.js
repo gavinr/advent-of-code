b1cac48cf9379c85db7d3a044fd462b75b26b697
Gavin Rehkemper <gavin@gavinr.com>
2019-12-16 18:27:34 -0600

    updates for speed. thanks kate

--- 2019/day16/part1.js
+++ 2019/day16/part1.js
@@ -1,50 +1,41 @@
 // prettier-ignore
 const input = '59738476840592413842278931183278699191914982551989917217627400830430491752064195443028039738111788940383790187992349338669216882218362200304304999723624146472831445016914494176940890353790253254035638361091058562936884762179780957079673204210602643442603213181538626042470133454835824128662952579974587126896226949714610624975813583386749314141495655816215568136392852888525754247201021383516228214171660111826524421384758783400220017148022332694799323429711845103305784628923350853888186977670136593067604033507812932778183786479207072226236705355778245701287481396364826358903409595200711678577506495998876303181569252680220083046665757597971122614';
 
-const getPhaseDigit = (inputString, digitNumber) => {
+// prettier-ignore
+// const input = '69317163492948606335995924319873';
+
+const getPhaseDigit = (inputString, n) => {
   const charArray = inputString.split('');
-  const basePattern = [0, 1, 0, -1];
-  const fullPhase = basePattern.reduce((acc, cur) => {
-    let arr = [];
-    for(let i = 0; i < digitNumber+1; i++) {
-      arr.push(cur);
+  let sum = 0;
+  for(let j = 1; j <= charArray.length/n; j = j+4) {
+    for(let k = (j * n); k <= ((j + 1) * n) - 1; k++) {
+      if(k <= charArray.length) {
+        const digit = parseInt(charArray[k-1]);
+        sum = sum + digit;
+      }
     }
-    return acc.concat(arr);
-  }, []);
-  
-  // start with the full phase but offset by one:
-  let phase = [...fullPhase];
-  phase.shift();
-
-  // now add on "fullPhases" until we have enough:
-  while(phase.length < charArray.length) {
-    phase = phase.concat(fullPhase);
   }
-  // console.log('phase:', phase);
 
-  // multiply each digit by each phase num
-  let retString = '';
-  const partsToBeAdded = charArray.map((digit, i) => {
-    return parseInt(digit) * phase[i];
-  });
+  for(let j = 3; j <= charArray.length/n; j = j+4) {
+    for(let k = (j * n); k <= ((j + 1) * n) - 1; k++) {
+      if(k <= charArray.length) {
+        const digit = parseInt(charArray[k-1]);
+        sum = sum - digit;
+      }
+    }
+  }
 
-  // add them all up:
-  const total = partsToBeAdded.reduce((acc, cur) => {
-    return acc + cur;
-  }, 0);
-
-  // take only the ones digit 
-  const onesDigit = String(total).charAt(String(total).length-1);
-
+   // take only the ones digit 
+  const onesDigit = String(sum).charAt(String(sum).length-1);
   return onesDigit;
 }
 
 const getPhase = (inputString) => {
   let retString = '';
-  for(let i = 0; i < inputString.length; i++) {
+  for(let i = 1; i <= inputString.length; i++) {
     retString = `${retString}${getPhaseDigit(inputString, i)}`
   }
-  console.log('retString', retString);
+  // console.log('retString', retString);
   return retString;
 }
 const runProgram = (input, numPhases, digits) => {
