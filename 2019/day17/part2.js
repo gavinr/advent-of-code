062bfdd17b2fbb181c75cd416e53f9092e94e425
Gavin Rehkemper <gavin@gavinr.com>
2019-12-18 23:13:23 -0600

    part 17 - working (sortof)

--- 2019/day17/part2.js
+++ 2019/day17/part2.js
@@ -1,7 +1,273 @@
 // prettier-ignore
-const input = [];
+var input = [2,330,331,332,109,3664,1102,1,1182,15,1101,0,1455,24,1001,0,0,570,1006,570,36,102,1,571,0,1001,570,-1,570,1001,24,1,24,1105,1,18,1008,571,0,571,1001,15,1,15,1008,15,1455,570,1006,570,14,21101,58,0,0,1105,1,786,1006,332,62,99,21102,333,1,1,21102,1,73,0,1105,1,579,1102,1,0,572,1101,0,0,573,3,574,101,1,573,573,1007,574,65,570,1005,570,151,107,67,574,570,1005,570,151,1001,574,-64,574,1002,574,-1,574,1001,572,1,572,1007,572,11,570,1006,570,165,101,1182,572,127,102,1,574,0,3,574,101,1,573,573,1008,574,10,570,1005,570,189,1008,574,44,570,1006,570,158,1105,1,81,21101,340,0,1,1105,1,177,21102,1,477,1,1106,0,177,21101,514,0,1,21102,1,176,0,1105,1,579,99,21102,1,184,0,1106,0,579,4,574,104,10,99,1007,573,22,570,1006,570,165,102,1,572,1182,21102,375,1,1,21101,211,0,0,1106,0,579,21101,1182,11,1,21102,222,1,0,1105,1,979,21102,1,388,1,21101,233,0,0,1105,1,579,21101,1182,22,1,21102,1,244,0,1105,1,979,21102,401,1,1,21101,0,255,0,1106,0,579,21101,1182,33,1,21101,266,0,0,1106,0,979,21101,414,0,1,21102,277,1,0,1105,1,579,3,575,1008,575,89,570,1008,575,121,575,1,575,570,575,3,574,1008,574,10,570,1006,570,291,104,10,21101,0,1182,1,21102,313,1,0,1105,1,622,1005,575,327,1102,1,1,575,21102,1,327,0,1105,1,786,4,438,99,0,1,1,6,77,97,105,110,58,10,33,10,69,120,112,101,99,116,101,100,32,102,117,110,99,116,105,111,110,32,110,97,109,101,32,98,117,116,32,103,111,116,58,32,0,12,70,117,110,99,116,105,111,110,32,65,58,10,12,70,117,110,99,116,105,111,110,32,66,58,10,12,70,117,110,99,116,105,111,110,32,67,58,10,23,67,111,110,116,105,110,117,111,117,115,32,118,105,100,101,111,32,102,101,101,100,63,10,0,37,10,69,120,112,101,99,116,101,100,32,82,44,32,76,44,32,111,114,32,100,105,115,116,97,110,99,101,32,98,117,116,32,103,111,116,58,32,36,10,69,120,112,101,99,116,101,100,32,99,111,109,109,97,32,111,114,32,110,101,119,108,105,110,101,32,98,117,116,32,103,111,116,58,32,43,10,68,101,102,105,110,105,116,105,111,110,115,32,109,97,121,32,98,101,32,97,116,32,109,111,115,116,32,50,48,32,99,104,97,114,97,99,116,101,114,115,33,10,94,62,118,60,0,1,0,-1,-1,0,1,0,0,0,0,0,0,1,46,22,0,109,4,2102,1,-3,587,20102,1,0,-1,22101,1,-3,-3,21102,1,0,-2,2208,-2,-1,570,1005,570,617,2201,-3,-2,609,4,0,21201,-2,1,-2,1106,0,597,109,-4,2105,1,0,109,5,2102,1,-4,629,21002,0,1,-2,22101,1,-4,-4,21101,0,0,-3,2208,-3,-2,570,1005,570,781,2201,-4,-3,652,21001,0,0,-1,1208,-1,-4,570,1005,570,709,1208,-1,-5,570,1005,570,734,1207,-1,0,570,1005,570,759,1206,-1,774,1001,578,562,684,1,0,576,576,1001,578,566,692,1,0,577,577,21101,702,0,0,1105,1,786,21201,-1,-1,-1,1105,1,676,1001,578,1,578,1008,578,4,570,1006,570,724,1001,578,-4,578,21102,731,1,0,1106,0,786,1106,0,774,1001,578,-1,578,1008,578,-1,570,1006,570,749,1001,578,4,578,21102,1,756,0,1105,1,786,1105,1,774,21202,-1,-11,1,22101,1182,1,1,21102,774,1,0,1106,0,622,21201,-3,1,-3,1105,1,640,109,-5,2106,0,0,109,7,1005,575,802,20101,0,576,-6,21002,577,1,-5,1105,1,814,21101,0,0,-1,21101,0,0,-5,21102,0,1,-6,20208,-6,576,-2,208,-5,577,570,22002,570,-2,-2,21202,-5,47,-3,22201,-6,-3,-3,22101,1455,-3,-3,2102,1,-3,843,1005,0,863,21202,-2,42,-4,22101,46,-4,-4,1206,-2,924,21102,1,1,-1,1106,0,924,1205,-2,873,21101,0,35,-4,1105,1,924,1202,-3,1,878,1008,0,1,570,1006,570,916,1001,374,1,374,2101,0,-3,895,1101,2,0,0,1201,-3,0,902,1001,438,0,438,2202,-6,-5,570,1,570,374,570,1,570,438,438,1001,578,558,921,21001,0,0,-4,1006,575,959,204,-4,22101,1,-6,-6,1208,-6,47,570,1006,570,814,104,10,22101,1,-5,-5,1208,-5,47,570,1006,570,810,104,10,1206,-1,974,99,1206,-1,974,1101,0,1,575,21101,0,973,0,1105,1,786,99,109,-7,2105,1,0,109,6,21102,0,1,-4,21101,0,0,-3,203,-2,22101,1,-3,-3,21208,-2,82,-1,1205,-1,1030,21208,-2,76,-1,1205,-1,1037,21207,-2,48,-1,1205,-1,1124,22107,57,-2,-1,1205,-1,1124,21201,-2,-48,-2,1105,1,1041,21101,0,-4,-2,1105,1,1041,21101,-5,0,-2,21201,-4,1,-4,21207,-4,11,-1,1206,-1,1138,2201,-5,-4,1059,2102,1,-2,0,203,-2,22101,1,-3,-3,21207,-2,48,-1,1205,-1,1107,22107,57,-2,-1,1205,-1,1107,21201,-2,-48,-2,2201,-5,-4,1090,20102,10,0,-1,22201,-2,-1,-2,2201,-5,-4,1103,2101,0,-2,0,1105,1,1060,21208,-2,10,-1,1205,-1,1162,21208,-2,44,-1,1206,-1,1131,1105,1,989,21101,0,439,1,1105,1,1150,21102,1,477,1,1106,0,1150,21102,1,514,1,21102,1,1149,0,1105,1,579,99,21101,0,1157,0,1106,0,579,204,-2,104,10,99,21207,-3,22,-1,1206,-1,1138,2102,1,-5,1176,2101,0,-4,0,109,-6,2106,0,0,14,5,42,1,3,1,42,1,3,1,42,1,3,1,42,1,3,1,42,1,3,1,42,1,3,1,5,13,24,1,3,1,5,1,11,1,24,1,1,5,3,1,1,13,22,1,1,1,1,1,1,1,3,1,1,1,9,1,1,1,22,1,1,1,1,13,5,1,1,1,22,1,1,1,3,1,3,1,1,1,3,1,5,1,1,1,22,7,3,1,1,1,1,11,24,1,7,1,1,1,1,1,1,1,5,1,18,9,7,1,1,5,5,5,14,1,15,1,3,1,11,1,14,1,15,1,3,1,11,1,14,1,15,1,3,1,11,1,6,9,5,11,3,1,11,1,6,1,13,1,13,1,11,1,6,1,13,1,9,7,9,1,6,1,13,1,9,1,3,1,1,1,9,1,6,1,13,13,1,1,1,1,9,8,23,1,1,1,1,1,1,1,16,1,23,5,1,1,16,1,25,1,3,1,16,1,25,1,3,1,16,1,25,1,3,1,16,1,25,1,3,1,16,1,25,1,3,1,16,7,19,1,3,1,22,1,19,1,3,1,22,1,19,5,22,1,46,1,46,1,46,1,46,1,46,5,46,1,46,1,46,1,46,1,46,1,46,1,46,1,46,13,24
+];
 
-const runProgram = (input) => {
-  return 'todo: implement';
+
+
+var pad = function(n, width, z) {
+  z = z || "0";
+  n = n + "";
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 };
-console.log("RESULT: ", runProgram(input));
+
+const intcodeRun = (inn, a) => {
+  var input = [...inn]; // clone
+  var currentLocation = 0;
+  var relativeBase = 0;
+  let fullOutput = [[]];
+
+
+  const getParam = (param_num) => {
+    var imode = input[currentLocation];
+    var val = input[currentLocation + param_num];
+    var parameterString = pad(imode, 5);
+
+    var mode;
+    if(param_num === 1) {
+      mode = parameterString.charAt(2);
+    } else if (param_num === 2) {
+      mode = parameterString.charAt(1);
+    } else if (param_num === 3) {
+      mode = parameterString.charAt(0);
+    }
+
+    if(mode === "0") {
+      return input[val]
+    } else if (mode === "1") {
+      return val;
+    } else if (mode === "2") {
+      return input[val + relativeBase];
+    }
+  }
+  
+  const setParam = (param_num, set_to) => {
+    var imode = input[currentLocation];
+    var val = input[currentLocation + param_num];
+    var parameterString = pad(imode, 5);
+
+    var mode;
+    if(param_num === 1) {
+      mode = parameterString.charAt(2);
+    } else if (param_num === 2) {
+      mode = parameterString.charAt(1);
+    } else if (param_num === 3) {
+      mode = parameterString.charAt(0);
+    }
+
+    if(mode === "0") {
+      input[val] = set_to;
+    } else if (mode === "1") {
+      console.error('ERROR SHOULD NOT BE HERE');
+    } else if (mode === "2") {
+      input[val + relativeBase] = set_to;
+    }
+  }
+
+  var count = 0;
+  while (input[currentLocation] !== 99 && count < 20000000) {
+    var currentValue = input[currentLocation];
+    var parameterString = pad(currentValue, 5);
+    var opCode = parseInt(parameterString[4]);
+
+    if (opCode === 1) {
+      // add
+      setParam(3, getParam(1) + getParam(2));
+      currentLocation = currentLocation + 4;
+    } else if (opCode === 2) {
+      // multiply
+      setParam(3, getParam(1) * getParam(2));
+
+      currentLocation = currentLocation + 4;
+    } else if (opCode === 3) {
+      console.log(asciiArrayToString(fullOutput[fullOutput.length-1]));
+      fullOutput.push([]);
+      // input
+      const a1 = a.shift();
+      console.log('a1:', a1);
+      setParam(1, a1);
+      currentLocation = currentLocation + 2;
+    } else if (opCode === 4) {
+      // console.log("--------------------- OUTPUT:", getParam(1));
+
+      // read in last output:
+      // if(fullOutput[fullOutput.length-1].length > 2256) {
+        
+      // }
+      
+
+      fullOutput[fullOutput.length-1].push(getParam(1));
+      // return getParam(1);
+      currentLocation = currentLocation + 2;
+    } else if (opCode === 5) {
+      // jump-if-true
+      if (getParam(1) !== 0) {
+        currentLocation = getParam(2);
+      } else {
+        currentLocation = currentLocation + 3;
+      }
+    } else if (opCode === 6) {
+      // jump if false
+      if (getParam(1) === 0) {
+        currentLocation = getParam(2);
+      } else {
+        currentLocation = currentLocation + 3;
+      }
+    } else if (opCode === 7) {
+      // less than
+      if (getParam(1) < getParam(2)) {
+        setParam(3, 1);
+      } else {
+        setParam(3, 0);
+      }
+      currentLocation = currentLocation + 4;
+    } else if (opCode === 8) {
+      // equals
+      if (getParam(1) === getParam(2)) {
+        setParam(3, 1);
+      } else {
+        setParam(3, 0);
+      }
+      currentLocation = currentLocation + 4;
+    } else if (opCode === 9) {
+      // modify relative base
+      relativeBase = relativeBase + getParam(1); // input[currentLocation + 1];
+      currentLocation = currentLocation + 2;
+    } else {
+      console.log("error - opCode:", opCode);
+      break;
+    }
+    count = count + 1;
+  }
+  console.log(asciiArrayToString(fullOutput[fullOutput.length-1]));
+  return fullOutput;
+};
+
+var grid = {};
+
+const toGrid = (input) => {
+  let currentRow = 0;
+  grid[0] = {};
+  let currentCol = 0;
+  for(let i = 0; i < input.length; i++) {
+    const val = input[i];
+    if(val === 46) {
+      grid[currentRow][currentCol] = '.';
+      currentCol = currentCol + 1;
+    } else if(val === 35) {
+      grid[currentRow][currentCol] = '#';
+      currentCol = currentCol + 1;
+    } else if(val === 94) {
+      grid[currentRow][currentCol] = '^';
+      currentCol = currentCol + 1;
+    } else if(val === 10) {
+      currentRow = currentRow + 1;
+      currentCol = 0;
+      grid[currentRow] = {};
+    } else {
+      console.error('Error - found letter we do not know!', val);
+    }
+  }
+}
+
+const render = grid => {
+  const tableDomNode = document.getElementById("gameTable");
+  while (tableDomNode.firstChild) {
+    tableDomNode.removeChild(tableDomNode.firstChild);
+  }
+  const widthHeight = "font-size: 10px; width: 10px; height: 10px; text-align: center;";
+
+  let currentRow = document.createElement("tr");
+  tableDomNode.append(currentRow);
+
+  Object.keys(grid).forEach((row) => {
+    currentRow = document.createElement("tr");
+    tableDomNode.append(currentRow);
+
+    Object.keys(grid[row]).forEach((col) => {
+      const td = document.createElement("td");
+      td.innerHTML = grid[row][col];
+      td.style.cssText = widthHeight;
+      if(grid[row][col] === '.') {
+        td.style["background-color"] = "white";
+      } else if(grid[row][col] === '#') {
+        td.style["background-color"] = "red";
+      } else if(grid[row][col] === '^') {
+        td.style["background-color"] = "lightgreen";
+      }
+      currentRow.appendChild(td);
+    });
+  });
+};
+
+const findIntersections = (grid) => {
+  let retIntersections = [];
+  Object.keys(grid).forEach((row) => {
+    Object.keys(grid[row]).forEach((col) => {
+      if(
+        grid.hasOwnProperty(parseInt(row)-1) &&
+        grid.hasOwnProperty(parseInt(row)+1) &&
+        grid[row].hasOwnProperty(parseInt(col)-1) && 
+        grid[row].hasOwnProperty(parseInt(col)+1)
+      ) {
+        // console.log('checking: ', `${col}, ${row}`, ':', `${grid[row-1][col]} ${grid[row+1][col]} ${grid[row][col-1]} ${grid[row][col+1]}`);
+        if(
+          grid[row][col] === '#' &&
+          grid[parseInt(row)-1][col] === '#' &&
+          grid[parseInt(row)+1][col] === '#' &&
+          grid[row][parseInt(col)-1] === '#' &&
+          grid[row][parseInt(col)+1] === '#'
+        ) {
+          retIntersections.push([parseInt(col), parseInt(row)]);
+        }
+      }
+    });
+  });
+  return retIntersections;
+}
+
+const toAscii = (str) => {
+  let retArr = [];
+  for(let i = 0; i < str.length; i++) {
+    retArr.push(str.charCodeAt(i));
+  }
+  return retArr;
+}
+
+const asciiArrayToString = (arr) => {
+  let retStr = '';
+  arr.forEach((num) => {
+    retStr = `${retStr}${String.fromCharCode(num)}`;
+  });
+  return retStr;
+}
+
+
+const runProgram = (inn) => {
+  // const fullOutput = intcodeRun(inn);
+  // // console.log('fullOutput', fullOutput);
+  // toGrid(fullOutput);
+  // // console.log('grid:', grid);
+  // render(grid);
+  // const intersections = findIntersections(grid);
+  // // console.log('intersections', intersections);
+  // return intersections.reduce((acc, cur) => {
+  //   return acc + (cur[0] * cur[1]);
+  // }, 0);
+  const mvmtString = toAscii('B,C,C,A,C,A,C,A,B,B');
+  console.log('mvmtString:', mvmtString);
+  const mvmtFunctionA = toAscii('L,12,L,6,L,4,L,4');
+  console.log('mvmtFunctionA:', mvmtFunctionA);
+
+  const mvmtFunctionB = toAscii('L,6,R,8,L,4,R,8,L,12');
+  console.log('mvmtFunctionB:', mvmtFunctionB);
+
+  const mvmtFunctionC = toAscii('L,12,R,10,L,4');
+  console.log('mvmtFunctionC:', mvmtFunctionC);
+
+  // only works in "live" mode?
+  // https://www.reddit.com/r/adventofcode/comments/ec5dst/2019_day_17_part_2_no_output_from_the_vac_bot/
+  const result = intcodeRun(inn, [... mvmtString, 10, ... mvmtFunctionA, 10, ... mvmtFunctionB, 10, ... mvmtFunctionC, 10, 89, 10]); // 110
+  return result;
+}
+
+console.log("RESULT: ", runProgram(input)); // answer: 4220
