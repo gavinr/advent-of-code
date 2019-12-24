eaa2dc2885a8f71083785460da7c7717b7c06ed1
Gavin Rehkemper <gavin@gavinr.com>
2019-12-23 20:44:29 -0600

    in progress

--- 2019/day20/part1.js
+++ 2019/day20/part1.js
@@ -11,21 +11,53 @@ const isMazeName = (character) => {
   return character.match(/^[A-Z]*$/);
 }
 
-const runProgram = async (inputFileName) => {
-  const mazeArr = await getInput(inputFileName);
+const checkCross = (value, x, y) => {
+  if(isMazeName(grid[y-1][x]) ) {
+    // up 
+    return {
+      location: [x, y],
+      name: `${grid[y-1][x]}${value}`
+    }
+  } else if(isMazeName(grid[y][x+1])) {
+    // right
+    return {
+      location: [x, y],
+      name: `${value}${grid[y][x+1]}`
+    }
+  } else if (isMazeName(grid[y-1][x])) {
+    // down
+    return {
+      location: [x, y],
+      name: `${value}${grid[y][x+1]}`
+    }
+  } else if (isMazeName(grid[y][x-1])) {
+    // left
+  } else {
+    console.error('ERROR', value, x, y);
+  }
+}
 
-  const portals = {};
+let mazeArr;
+
+const runProgram = async (inputFileName) => {
+  mazeArr = await getInput(inputFileName);
+
+  const portals = [];
 
   for (let y = 0; y < mazeArr.length; y++) {
     for (let x = 0; x < mazeArr[y].length; x++) {
       const value = mazeArr[y][x];
       if(isMazeName(value)) {
         console.log('get entry', value);
+        const fullValue = checkCross(value, x, y);
+        portals.push(fullValue);
       } else {
         console.log('not an entry', value);
       }
     }
   }
+
+  console.log('portals: ', portals);
 
   return 'todo: implement';
 };
